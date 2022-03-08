e90a4574926caf0e127a6c796898f8e598457e23
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2022-03-08 10:45:54 +0700

    Update Home
    + Card annoucement pada home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  announcements: any;
+  announcementsHomeDate: Date;
+  i: number;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.http.get('./assets/wsdc_data.json').subscribe((data: any) => {
+      this.announcements = data;
+      this.announcementsHomeDate = data.localtime;
+    });
   }
 
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+
+  formatDatetime(sqlDatetime: string) {
+    // Note custom function is preferred over libraries, that contains many unnecessary codes.
+    if (sqlDatetime === null) {
+      return null;
+    }
+    var date = new Date(sqlDatetime.substr(0, 10));
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayOfWeek = date.getDay();
+    return dayNames[dayOfWeek] + ', ' + sqlDatetime.substr(11, 5);
+  }
+
+  getAnnouncementHome() {
+    return this.announcementsHomeDate;
+  }
 }
