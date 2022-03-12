de7d98bc51d39df01619c28d49431749c113b7d3
Rajasa Cikal Maulana Solihin <79238217+cikalmaulana@users.noreply.github.com>
2022-03-12 12:43:21 +0700

    Update Venues Map
    + Google Maps API included

--- src/app/pages/venues-map/venues-map.page.ts
+++ src/app/pages/venues-map/venues-map.page.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+
+declare var google: any;
+
 @Component({
   selector: 'app-venues-map',
   templateUrl: './venues-map.page.html',
@@ -11,6 +14,9 @@ export class VenuesMapPage implements OnInit {
   venuesPage: any;
   venuesMaps: any;
   filterVenue: any;
+  map: any;
+
+  @ViewChild('map', {read:ElementRef, static: false}) mapRef: ElementRef;
   constructor(private http: HttpClient, private actovatedRoute: ActivatedRoute, private router: Router) {
     // this.actovatedRoute.paramMap.subscribe(
     //   (data) => {
@@ -19,13 +25,27 @@ export class VenuesMapPage implements OnInit {
     // )
 
     this.venuesPage = this.actovatedRoute.snapshot.paramMap.get('var')
-    this.http.get('./assets/json/venues.json').subscribe((data: any) => {
-      this.venuesMaps = data;
+    this.http.get('https://wsdc.dnartworks.com/wsdc_data.json').subscribe((data: any) => {
+      this.venuesMaps = data.venues;
     });
     this.memfilter();
   }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter(){
+    this.showMap(); 
+  }
+
+  showMap(){
+    const location = new google.maps.LatLng(-17.824858, 31.053028);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
   }
 
   check(id) {
