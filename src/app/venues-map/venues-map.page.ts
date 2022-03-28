7e9a2fdd478536ce21671c356aa58db7abf80e38
Rajasa Cikal Maulana Solihin <79238217+cikalmaulana@users.noreply.github.com>
2022-03-28 12:48:00 +0700

    Update Venues_map
    + Change Google Maps to Capacitor Google Maps (android native)

--- src/app/venues-map/venues-map.page.ts
+++ src/app/venues-map/venues-map.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
 
 declare var google: any;
 
@@ -14,56 +15,81 @@ declare var google: any;
 export class VenuesMapPage implements OnInit {
   venuesPage: any;
   venuesMaps: any;
+  venuesMapsDetail: any;
   filterVenue: any;
   map: any;
   coordinates: any;
-
-  @ViewChild('map', {read:ElementRef, static: false}) mapRef: ElementRef;
+  @ViewChild('map') mapView: ElementRef;
+  // @ViewChild('map', {read:ElementRef, static: false}) mapRef: ElementRef;
   constructor(private http: HttpClient, private actovatedRoute: ActivatedRoute, private router: Router,private storage: Storage) {
-    // this.actovatedRoute.paramMap.subscribe(
-    //   (data) => {
-    //     this.venuesPage = data;
-    //   }
-    // )
-
     // Ambil param yang dikirim dari venues.ts
     // 'var' ada di app-routing.module.ts
     this.venuesPage = this.actovatedRoute.snapshot.paramMap.get('var')
-    // this.http.get('https://wsdc.dnartworks.com/wsdc_data.json').subscribe((data: any) => {
-    //   this.venuesMaps = data.venues;
-    // });
     this.storage.get('wsdcDataStorage').then((data) => {
       console.log("Masuk Venues Map");
       this.venuesMaps = data.venues;
+      this.venuesMapsDetail = this.venuesMaps.filter(d=> d.id==this.venuesPage);
     })
-    this.memfilter();
   }
 
   ngOnInit() {
   }
 
-  ionViewDidEnter(){
-    this.showMap(-8.409518, 115.188919); //first lat long is in bali
+  async loadMap(lat, long){
+    
+
+    const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
+
+    CapacitorGoogleMaps.create({
+      width: Math.round(boundingRect.width),
+      height: Math.round(boundingRect.height),
+      x: Math.round(boundingRect.x),
+      y: Math.round(boundingRect.y),
+      latitude: -8.409518, //lat bali
+      longitude: 115.188919, //long bali
+      zoom: 10
+    });
+    
+      
+      for(let venue of this.venuesMaps){
+        if(this.venuesPage == venue.id){
+          for(let venuesMarker of venue.geojson.features){
+            const koor = venuesMarker.geometry.coordinates;
+              CapacitorGoogleMaps.addListener("onMapReady", async function() {
+
+                /*
+                  We can do all the magic here when map is ready
+                */
+                  
+                    CapacitorGoogleMaps.addMarker({
+                      latitude: koor[1],
+                      longitude: koor[0],
+                      title: venuesMarker.properties.Name
+                    });
+
+                    CapacitorGoogleMaps.enableCurrentLocation({
+                      enabled:true
+                    });
+
+                    CapacitorGoogleMaps.setMapType({
+                      "type": "normal"
+                    })  
+                    
+                })
+          }
+        }
+      }
   }
 
-  showMap(lat, lng){ 
-    const location = new google.maps.LatLng(lat,lng);
-    const options = {
-      center: location,
-      zoom: 15,
-      disableDefaultUI: true
-    }
-    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  async ionViewDidEnter() {
+    this.loadMap(-8.409518,115.188919); //Bali latitude and longitude
+  }
 
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(lat,lng),
-        map: this.map
-    });
-
-    new google.maps.Marker({
-      position: location,
-      title: "Hello World!",
-    });
+  ionViewDidLeave() {
+    CapacitorGoogleMaps.addListener("onMapReady", async function() {
+      CapacitorGoogleMaps.clear();
+    })
+    CapacitorGoogleMaps.close();
   }
 
   check(id) {
@@ -72,19 +98,29 @@ export class VenuesMapPage implements OnInit {
     }
   }
 
-  memfilter() {
-    this.filterVenue = this.venuesMaps;
-    // this.filterVenue = this.venuesMaps.filter(this.venuesMaps.id == this.venuesPage);
-  }
-
   backToVenue() {
-    this.router.navigate(['venues'])
+    CapacitorGoogleMaps.addListener("onMapReady", async function() {
+      CapacitorGoogleMaps.clear();
+    })
+    CapacitorGoogleMaps.close();
+    this.router.navigate(['venues']);
   }
 
   // Method untuk menset latitude dan longitude dari suatu tempat
-  setMapDetail(coordinates){
+  async setMapDetail(coordinates){
     this.coordinates = coordinates;
     console.log(this.coordinates[0] + ' ' + this.coordinates[1]);
-    this.showMap(this.coordinates[1],this.coordinates[0]);
+    
+    CapacitorGoogleMaps.setCamera({
+      latitude: this.coordinates[1],
+      longitude: this.coordinates[0],
+      zoom: 15,
+      animate: true,
+      animationDuration: 12
+    })
+
+    CapacitorGoogleMaps.setMapType({
+      "type": "normal"
+    })
   }
 }
