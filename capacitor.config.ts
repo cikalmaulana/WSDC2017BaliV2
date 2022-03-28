e703b2a1ec5ea0324564fca28e52a6ba317b9106
Rajasa Cikal Maulana Solihin <79238217+cikalmaulana@users.noreply.github.com>
2022-03-28 12:44:34 +0700

    Update config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'WSDC2017BaliV2',
+  appName: 'WSDC 2017 Bali V2',
   webDir: 'www',
   bundledWebRuntime: false
 };
